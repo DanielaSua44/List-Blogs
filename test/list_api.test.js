e6ea8ff93aa3da3d991fc6a49b6e2e5df01d011c
Daniela <danumilonaria44@gmail.com>
2022-08-12 22:15:29 -0300

    update

--- test/list_api.test.js
+++ test/list_api.test.js
@@ -86,6 +86,46 @@ test('Ibentificador unco id',async () => {
     expect(response.body[0].id).toBeDefined()
 } )
 
+test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+} )
+
+test('succeds witch status code 200 if update is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    const updatedBlog = {
+        title: 'Updated blog',
+        author: 'Updated author',
+        url: 'www.updated.com',
+        likes: 0
+    }
+
+    await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).toContain('Updated blog')
+} )
+
 afterAll(() => {
     mongoose.connection.close()
 } )
